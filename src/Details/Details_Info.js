56828e977b3874eb09cb4c749b1cf79cae1ded72
cong196 <vancongct.96@gmail.com>
2017-09-27 02:34:25 +0700

    tien nghi hang dau

--- src/Details/Details_Info.js
+++ src/Details/Details_Info.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {
-    View, Text, ScrollView, TouchableOpacity
+    View, Text, ScrollView, TouchableOpacity, StyleSheet, Image
 } from 'react-native';
-import global from '../global'
+import global from '../global';
+
 export default class Details_Info extends Component {
 
     constructor(props) {
@@ -36,15 +37,68 @@ export default class Details_Info extends Component {
     }
     render(){
         return(
-            <View style = {{ flex: 1 }}>
-                <ScrollView>
-                    <View>
-                        <TouchableOpacity>
-                            <Text>{global.idKS}</Text>
-                        </TouchableOpacity>
+            <ScrollView>
+                <View style={{ padding: 7 }}>
+                    <Text style={styles.textHead}>Tiện nghi hàng đầu</Text>
+                    <View style={styles.row}>
+                        <View style={[styles.item, {  }]}>
+                            <Image source={require('./img/icwifi.png')} style={styles.imageItem} />
+                            <View style = {{ flex: 4 }}>
+                                <Text numberOfLines={1}>Wifi tại sảnh</Text>
+                            </View>
+                        </View>
+
+                        <View style={[styles.item, {  }]}>
+                            <Image source={require('./img/icwifi_active.png')} style={styles.imageItem} />
+                            <View style = {{ flex: 4 }}>
+                                <Text numberOfLines={1}>Wifi trong phòng</Text>
+                            </View>
+                        </View>
                     </View>
-                </ScrollView>
-            </View>
+
+
+                    <View style={styles.row}>
+                        <View style={[styles.item, {  }]}>
+                            <Image source={require('./img/icpool.png')} style={styles.imageItem} />
+                            <View style = {{ flex: 4 }}>
+                                <Text numberOfLines={1}>Bể bơi</Text>
+                            </View>
+                        </View>
+
+                        <View style={[styles.item, {  }]}>
+                            <Image source={require('./img/icspa_active.png')} style={styles.imageItem} />
+                            <View style = {{ flex: 4 }}>
+                                <Text numberOfLines={1}>Spa</Text>
+                            </View>
+                        </View>
+                    </View>
+
+
+                </View>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    textHead: {
+        color: '#3669b5'
+    },
+    row: {
+        paddingVertical: 10,
+        flexDirection: 'row'
+    },
+    item: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        
+    },
+    imageItem: {
+        height: 25,
+        width: 25,
+        marginHorizontal: 10,
+        flex: 1,
+        resizeMode: 'contain'
+    }
+});
--- src/Details/Ratings.js
+++ src/Details/Ratings.js
@@ -7,23 +7,39 @@ export default class Ratings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mang: [],
+            mang: [{"id":1,"ten":"James Nguyễn","noidung":"Tốt"},{"id":3,"ten":"Messi","noidung":"Đẹp đẹp đẹp đẹp"},{"id":4,"ten":"Ronaldo","noidung":"Chất lượng ngon ngon ngon ngon"},{"id":5,"ten":"Nguyễn Văn Nhật","noidung":"Siêu siêu siêu soee"},{"id":6,"ten":"Chung Nguyen","noidung":"adb is not recognized as an internal or external command,operable program or batch file"}],
             value: '',
-            height: 40
+            height: 40,
+            dangnhap : false
         }
     }
     
     post() {
-        this.setState({
-            value: ''
-        })
+        if(this.state.value != ''){
+            if(!this.state.dangnhap)
+            {
+                const { mang } = this.state;
+                var maxId = Math.max.apply(null, mang.map(item => item.id)) + 1;
+                mang.unshift({ id: maxId, ten: 'ABC', noidung: this.state.value });
+                this.setState({
+                    mang,
+                    value: '',
+                })
+            }
+            else{
+                alert('Bạn chưa đăng nhập .. ')
+            }
+        }else {
+            alert('Nhập nội dung !!')
+        }
+            
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: 'white' }}>
                 <FlatList
                     ListHeaderComponent={(
-                        <View>
+                        <View style={{ paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: '#e9ebee' }}>
                         <View style={{ flexDirection: 'row', padding: 10 }}>
                             <Image source={iconaccount} style={{ width: 25, height: 25 }} />
                             <View style={{ borderWidth: 1, flex: 1, marginLeft: 10, borderRadius: 5, borderColor: '#e9ebee' }}>
@@ -48,6 +64,25 @@ export default class Ratings extends Component {
                                     <Text style ={{ padding: 10, paddingLeft: 10, backgroundColor: '#4267b2', borderRadius: 5, color: 'white' }}>   Đăng   </Text>
                                 </TouchableOpacity>
                         </View>
+                        </View>
+                    )}
+                    data={this.state.mang}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) =>
+                    
+                    <View key={item.id} style={{ paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: '#e9ebee' }}>
+                        <View style={{ flexDirection: 'row', padding: 10 }}>
+                            <Image source={iconaccount} style={{ width: 25, height: 25 }} />
+                            <View style={{ borderWidth: 1, padding: 5, flex: 1, marginLeft: 5, borderRadius: 5, borderColor: '#e9ebee' }}>
+                                <Text style = {{ fontWeight: 'bold' }}>{item.ten}</Text>
+                                <Text>{item.noidung}</Text>
+                            </View>
+                        </View>
+                    </View>
+                    }
+                    ListFooterComponent={(
+                        <View style= {{ padding: 10 }}>
+                            <Text style={{ color: '#4267b2' }}>Xem thêm bình luận ...</Text>
                         </View>
                     )}
                 />
